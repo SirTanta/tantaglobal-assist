f86487b789c357706a9df1f920cbe18daecd8ca9
Jon Edwards <jedwards@tanta-holdings.com>
2026-06-20 12:55:04 -0600

    seo: explicit allow for GPTBot, ClaudeBot, Google-Extended, PerplexityBot in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,13 @@ import { site } from '@/lib/seo';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      { userAgent: '*',              allow: '/' },
+      { userAgent: 'GPTBot',         allow: '/' },
+      { userAgent: 'ClaudeBot',      allow: '/' },
+      { userAgent: 'Google-Extended', allow: '/' },
+      { userAgent: 'PerplexityBot',  allow: '/' },
+    ],
     sitemap: `${site.url}/sitemap.xml`,
   };
 }
